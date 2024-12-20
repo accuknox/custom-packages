360bbe5236bf74681ffa0f2789883b9f1e01427d
Rudraksh Pareek <rudraksh@accuknox.com>
2024-12-20 16:03:28 +0530

    chore: ssh-keyscan
    Signed-off-by: Rudraksh Pareek <rudraksh@accuknox.com>

--- .github/scripts/idt-release.sh
+++ .github/scripts/idt-release.sh
@@ -23,7 +23,7 @@ echo "Compiling version $VERSION"
 # compile system monitor on profiling-vm (Linux 5.4.0-42-generic)
 SSH_COMMAND="sudo docker run --rm --name=kubearmor-init --privileged -v '/tmp:/opt/kubearmor/BPF:rw' -v '/lib/modules:/lib/modules:ro' -v '/sys/kernel/security:/sys/kernel/security:ro' -v '/sys/kernel/debug:/sys/kernel/debug:ro' -v '/media/root/etc/os-release:/media/root/etc/os-release:ro' -v '/usr/src:/usr/src' kubearmor/kubearmor-init:${VERSION}; echo MD5SUM; md5sum /tmp/system_monitor.bpf.o"
 
-echo "yes" | ssh -p 18022 -i $SSH_KEY vagrant@vm.accuknox.com -C $SSH_COMMAND
+ssh -p 18022 -i $SSH_KEY vagrant@vm.accuknox.com -C $SSH_COMMAND
 
 scp -P 18022 -i $SSH_KEY vagrant@vm.accuknox.com:/tmp/system_monitor.bpf.o /tmp/system_monitor.bpf.o
 
