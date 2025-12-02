e93ae722b3dcfe33d6955daab41a6657ec9318f2
Aryan-sharma11 <aryan1126.sharma@gmail.com>
2025-12-02 15:36:34 +0530

    test
    Signed-off-by: Aryan-sharma11 <aryan1126.sharma@gmail.com>

--- .github/scripts/idt-release.sh
+++ .github/scripts/idt-release.sh
@@ -23,9 +23,9 @@ echo "Compiling version $VERSION"
 # compile system monitor on profiling-vm (Linux 5.4.0-42-generic)
 SSH_COMMAND="sudo docker run --rm --name=kubearmor-init --privileged -v '/tmp:/opt/kubearmor/BPF:rw' -v '/lib/modules:/lib/modules:ro' -v '/sys/kernel/security:/sys/kernel/security:ro' -v '/sys/kernel/debug:/sys/kernel/debug:ro' -v '/media/root/etc/os-release:/media/root/etc/os-release:ro' -v '/usr/src:/usr/src' kubearmor/kubearmor-init:${VERSION}; echo MD5SUM; md5sum /tmp/system_monitor.bpf.o"
 
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p 18022 -i $SSH_KEY 159.89.175.183 -C $SSH_COMMAND
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $SSH_KEY 159.89.175.183 -C $SSH_COMMAND
 
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -P 18022 -i $SSH_KEY 159.89.175.183:/tmp/system_monitor.bpf.o /tmp/system_monitor.bpf.o
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $SSH_KEY 159.89.175.183:/tmp/system_monitor.bpf.o /tmp/system_monitor.bpf.o
 
 echo "MD5SUM of COPIED SYSTEM MONITOR"
 md5sum /tmp/system_monitor.bpf.o
@@ -44,7 +44,6 @@ tar xvzf ${KA_TAR}
 rm -rf ${KA_TAR}
 cp /tmp/system_monitor.bpf.o opt/kubearmor/BPF/system_monitor.bpf.o
 tar czf ${KA_TAR} opt/ usr/
-
-# oras push --artifact-type=application/x-tar docker.io/kubearmor/kubearmor-systemd-${PKG_NAME}:${UNTAGGED_VERSION}_linux-amd64 ${KA_TAR}
+oras push --artifact-type=application/x-tar docker.io/kubearmor/kubearmor-systemd-${PKG_NAME}:${UNTAGGED_VERSION}_linux-amd64 ${KA_TAR}
 
 echo "COMPLETE!"
